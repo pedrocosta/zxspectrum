ca7c13b5d1bc21b1db1d17f9fdd856f3d21fdf23
Pedro Costa <eng.pedrocosta@gmail.com>
2026-06-12 20:17:25 +0100

    added game1

---
+++ game1.asm
@@ -0,0 +1,80 @@
+org $8000
+	ld a,71
+	ld (23693),a
+	xor a
+	call 8859
+	ld hl,blocks
+	ld (23675),hl
+	call 3503
+	ld hl,21+15*256
+	ld (plx),hl
+	call basexy
+	call splayr
+mloop	equ $
+	call basexy
+	call wspace
+	ld bc,63486
+	in a,(c)
+	rra
+	push af
+	call nc,mpl
+	pop af
+	rra
+	push af
+	call nc,mpr
+	pop af
+	rra
+	push af
+	call nc,mpd
+	pop af
+	rra
+	call nc,mpu
+	call basexy
+	call splayr
+	jp mloop
+mpl	ld hl,ply
+	ld a,(hl)
+	and a
+	ret z
+	dec (hl)
+	ret
+mpr	ld hl,ply
+	ld a,(hl)
+	cp 31
+	ret z
+	inc (hl)
+	ret
+mpu	ld hl,plx
+	ld a,(hl)
+	cp 4
+	ret z
+	dec (hl)
+	ret
+mpd	ld hl,plx
+	ld a,(hl)
+	cp 21
+	ret z
+	inc (hl)
+	ret
+basexy	ld a,22
+	rst 16
+	ld a,(plx)
+	rst 16
+	ld a,(ply)
+	rst 16
+	ret
+splayr	ld a,69
+	ld (23695),a
+	ld a,144
+	rst 16
+	ret
+wspace	ld a,71
+	ld (23695),a
+	ld a,32
+	rst 16
+	ret
+plx	defb 0
+ply	defb 0
+blocks	defb 16,16,56,56,124,124,254,254
+
+END $8000
